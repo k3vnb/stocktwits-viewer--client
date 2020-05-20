85f063eff7c709908155ed0a3b3996fba633b80e
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-05-20 05:35:43 -0700

    Refactor logic for auto-updating tweet streams Remove socket.io from project -- multiple bugs in this implementation causing too many api calls and janky rendering New version is simpler, cleaner, less overhead, more controllable results

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import socketIOClient from 'socket.io-client';
+import useInterval from './hooks/useInterval';
 import AppContext from './AppContext';
 import ChipContainer from './components/ChipContainer/ChipContainer';
 import SearchBar from './components/SearchBar/SearchBar';
 import TweetContainer from './components/TweetContainer/TweetContainer';
 import TweetPage from './components/TweetContainer/TweetPage';
-import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
-import ErrorPage from './components/ErrorPage/ErrorPage';
-import './App.css';
+// import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
 import EmptyPage from './components/EmptyPage/EmptyPage';
-
-const ENDPOINT = 'http://localhost:8000?params=';
+import './App.css';
 
 const App = () => {
-  const [response, setResponse] = useState('');
-  const [loading, setLoading] = useState(false);
+  const [syncData, setSyncData] = useState(0);
+  // const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
   const [tweetStream, setTweetStream] = useState([]);
   const [selectedSymbols, setSelectedSymbols] = useState([]);
 
   useEffect(() => {
-    const queryString = selectedSymbols.map(({ symbol }) => symbol).join(',');
     if (selectedSymbols.length) {
-      setLoading(true);
-      const socket = socketIOClient(`${ENDPOINT}/${queryString}`);
-      socket.on('FromAPI', (data) => {
-        if (data[0].response.status === 404) {
-          console.error('Could not retrieve data');
-          return setError('Could not retrieve data');
-        }
-        setResponse(data);
-      });
-      setTweetStream(() => response || [...tweetStream] || []);
-      setLoading(false);
+      const queryString = selectedSymbols.map(({ symbol }) => symbol).join('+');
+      const getUpdatedStream = async () =>
+        fetch(`http://localhost:8001/api/symbols/${queryString}`);
+
+      getUpdatedStream()
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          throw new Error('Could not fetch data');
+        })
+        .then((stream) => setTweetStream(stream || [...tweetStream]))
+        .catch((err) => setError(`Oops. ${err.message}`));
     }
-  }, [response, selectedSymbols]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [syncData]);
+
+  // calls to update the tweet streams will occur at this interval
+  useInterval(() => setSyncData(syncData + 1), 60000);
 
   const addSelectedSymbol = async (newSymbol) => {
     setSelectedSymbols([...selectedSymbols, newSymbol]);
@@ -74,7 +76,17 @@ const App = () => {
   const LandingPage = () => (
     <>
       <ChipContainer />
-      {tweetStream.length ? <TweetContainer /> : <EmptyPage />}
+      {tweetStream.length ? (
+        <TweetContainer />
+      ) : (
+        <EmptyPage
+          message={
+            error ||
+            'Select a stock from the searchbar to start watching stock tweets'
+          }
+        />
+      )}
+      {/* {loading && <LoadingSpinner />} */}
     </>
   );
 
@@ -90,8 +102,6 @@ const App = () => {
             <Route exact path="/" component={LandingPage} />
             <Route path="/symbol/:symbolId" component={TweetPage} />
           </Switch>
-          {loading && <LoadingSpinner />}
-          {error && <ErrorPage errorMessage={error} />}
         </main>
       </div>
     </AppContext.Provider>
