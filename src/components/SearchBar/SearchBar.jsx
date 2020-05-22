bb2423499c8a322afcfef301d527afa72c08fb16
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-05-21 19:03:31 -0700

    Major refactor to search bar Change nature of search bar whereby user can select multiple companies at once Checkbox is used for updating watchlist

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -13,16 +13,18 @@ import './SearchBar.css';
 
 const SearchBar = () => {
   const { setError } = useContext(AppContext);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerms, setSearchTerms] = useState([]);
+  const [resultsError, setResultsError] = useState('');
   const [showSearchResults, setShowSearchResults] = useState(false);
   const [searchResultsLoading, setSearchResultsLoading] = useState(false);
-  const [searchResults, setSearchResults] = useState([]);
+  const [results, setResults] = useState([]);
   const [searchResultPosition, setSearchResultPosition] = useState({
     top: 0,
     width: 0,
   });
   const searchInputRef = useRef(null);
 
+  // get position of searchbar to anchor flyout menu absolute position
   useEffect(() => {
     if (searchInputRef.current) {
       setSearchResultPosition({
@@ -45,47 +47,60 @@ const SearchBar = () => {
   }, []);
 
   useEffect(() => {
-    if (searchTerm.length > 1) {
-      return setShowSearchResults(true);
-    }
+    if (searchTerms.length) return setShowSearchResults(true);
     return setShowSearchResults(false);
-  }, [searchTerm]);
-
-  const setNewSearchString = (e) => {
-    setSearchTerm(e.target.value);
-    // character length is set to mitigate exceeding rate limit
-    if (searchTerm.length && searchTerm.length < 12) {
-      setSearchResultsLoading(true);
-      fetch(`${config.API_ENDPOINT}/search/${e.target.value}`)
-        .then((res) => {
-          if (res.ok) {
-            return res.json();
-          }
-          throw new Error('Could not fetch data');
-        })
-        .then(({ results }) => {
-          setSearchResults(results);
-          setSearchResultsLoading(false);
-        })
-        .catch((err) => {
-          setError(`Oops. ${err.message}`);
-          setSearchResultsLoading(false);
-        });
-    }
-  };
+  }, [searchTerms]);
 
   const toggleShowSearchResults = () =>
     setShowSearchResults(!showSearchResults);
 
-  const clearSearchTerm = () => setSearchTerm('');
+  const fetchData = () => {
+    if (searchTerms.length && searchTerms.every((term) => term.length)) {
+      setSearchResultsLoading(true);
+      setResultsError('');
+      const response = async () =>
+        Promise.all(
+          searchTerms.map((searchTerm) =>
+            fetch(`${config.API_ENDPOINT}/search/${searchTerm}`)
+              .then((res) => {
+                if (res.ok) {
+                  return res.json();
+                }
+                throw new Error('Could not fetch data');
+              })
+              .then(({ results }) => results)
+              .catch((err) => {
+                setError(`Oops. ${err.message}`);
+              })
+          )
+        );
+      response().then((symbolDataArr) => {
+        setSearchResultsLoading(false);
+        // make sure none of the values in the array are undefined
+        if (symbolDataArr.length && symbolDataArr.every((symbol) => symbol))
+          return !!symbolDataArr && setResults(symbolDataArr);
+        return setResultsError('Cannot fetch data, try again later');
+      });
+    }
+  };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => fetchData(), [searchTerms]);
 
+  const setNewSearchString = (e) => {
+    setSearchTerms(e.target.value.split(','));
+  };
+
+  const clearSearchTerms = () => setSearchTerms([]);
+
+  const flatMapResults = results.flatMap((result) => result);
   return (
     <div className="search-bar__container">
       <TextField
         inputRef={searchInputRef}
-        value={searchTerm}
+        value={searchTerms}
         fullWidth
         label="Search Stock Symbols"
+        title="Search multiple companies by inserting a comma (ie, 'apple,amazon')"
         variant="filled"
         onChange={setNewSearchString}
         autoFocus
@@ -94,11 +109,12 @@ const SearchBar = () => {
       {showSearchResults && (
         <SearchResultsFlyout
           position={searchResultPosition}
-          searchResults={searchResults}
+          searchResults={flatMapResults}
           toggleShowSearchResults={toggleShowSearchResults}
-          clearSearchTerm={clearSearchTerm}
-          setSearchResults={setSearchResults}
+          clearSearchTerm={clearSearchTerms}
+          setSearchResults={setResults}
           searchResultsLoading={searchResultsLoading}
+          errorMessage={resultsError}
         />
       )}
     </div>
--- src/components/SearchResultsFlyout/SearchResultsFlyout.jsx
+++ src/components/SearchResultsFlyout/SearchResultsFlyout.jsx
@@ -1,59 +1,95 @@
 import React, { useContext } from 'react';
+import { Checkbox, Button } from '@material-ui/core';
 import { shape, string, arrayOf, number, func, bool } from 'prop-types';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import { SmallLoadingSpinner } from '../LoadingSpinner/LoadingSpinner';
 import AppContext from '../../AppContext';
 import './SearchResultsFlyout.css';
 
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
 const AutoCompleteFlyout = ({
   searchResults,
-  setSearchResults,
   position: { top, width },
   toggleShowSearchResults,
   clearSearchTerm,
+  errorMessage,
   searchResultsLoading,
 }) => {
-  const { selectedSymbols, addSelectedSymbol } = useContext(AppContext);
-  const handleSelectSymbol = (symbol) => {
-    if (selectedSymbols.find(({ id }) => id === symbol.id)) {
-      return alert('This symbol has already been selected');
+  const {
+    selectedSymbols,
+    addSelectedSymbol,
+    removeSelectedSymbol,
+  } = useContext(AppContext);
+
+  const handleChecked = (e) => {
+    const symbolObj = JSON.parse(e.target.value);
+    if (selectedSymbols.some(({ id }) => id === symbolObj.id)) {
+      return removeSelectedSymbol(symbolObj.id);
     }
-    addSelectedSymbol(symbol);
+    return addSelectedSymbol(symbolObj);
+  };
+
+  const closeFlyout = () => {
     clearSearchTerm();
-    setSearchResults([]);
-    return toggleShowSearchResults();
+    toggleShowSearchResults();
   };
-  const handleKeyDown = (e, symbol) => {
-    if (e.key === 'Enter') {
-      return handleSelectSymbol(symbol);
-    }
-  };
+
   return (
     <ClickAwayListener onClickAway={toggleShowSearchResults}>
-      {!!searchResults && searchResults.length ? (
-        <div className="search-results__container" style={{ top, width }}>
-          {searchResults.map((symbolObj) => (
-            <div
-              className="results__list-item"
-              key={symbolObj.id}
-              onClick={() => handleSelectSymbol(symbolObj)}
-              onKeyDown={(e) => handleKeyDown(e, symbolObj)}
-              role="presentation"
-            >
-              <h4 className="results__list-item--title">{symbolObj.symbol}</h4>
-              <h5 className="results__list-item--subtitle">
-                {symbolObj.title}
-              </h5>
-            </div>
-          ))}
-          {searchResultsLoading && <SmallLoadingSpinner />}
-        </div>
-      ) : (
-        <div className="search-results__container" style={{ top, width }}>
-          No matching results found
-          {searchResultsLoading && <SmallLoadingSpinner />}
-        </div>
-      )}
+      <div className="search-results__container--outer">
+        {searchResults.length ? (
+          <div
+            className="search-results__container--inner"
+            style={{ top, width }}
+          >
+            {searchResults.map((symbolObj) => (
+              <div
+                className="results__list-item"
+                key={symbolObj.id}
+                role="presentation"
+              >
+                <Checkbox
+                  icon={icon}
+                  id="results-list-checkbox"
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selectedSymbols.some(
+                    ({ id }) => id === symbolObj.id
+                  )}
+                  value={JSON.stringify(symbolObj)}
+                  onChange={handleChecked}
+                />
+                <label
+                  htmlFor="results-list-checkbox"
+                  className="results__list-item--label"
+                >
+                  <span className="results__list-item--title">
+                    {`${symbolObj.symbol} - `}
+                  </span>
+                  <span className="results__list-item--subtitle">
+                    {symbolObj.title}
+                  </span>
+                </label>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div
+            className="search-results__container--error-message"
+            style={{ top, width }}
+          >
+            {errorMessage || 'No matching results found'}
+          </div>
+        )}
+        {searchResultsLoading && <SmallLoadingSpinner />}
+        <Button className="flyout-close-btn" onClick={closeFlyout}>
+          Close
+        </Button>
+      </div>
     </ClickAwayListener>
   );
 };
@@ -72,8 +108,8 @@ AutoCompleteFlyout.propTypes = {
     top: number.isRequired,
     width: number.isRequired,
   }).isRequired,
+  errorMessage: string.isRequired,
   toggleShowSearchResults: func.isRequired,
-  setSearchResults: func.isRequired,
   clearSearchTerm: func.isRequired,
   searchResultsLoading: bool.isRequired,
 };
